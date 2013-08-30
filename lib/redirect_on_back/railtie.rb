ce0de322cddc0212589d6bbb9545c8736143d870
Yossi Shasho <yossi@silo.co>
2013-08-30 16:39:43 +0300

    * can disabled redirect on back field * turbolinks support

--- lib/redirect_on_back/railtie.rb
+++ lib/redirect_on_back/railtie.rb
@@ -8,7 +8,10 @@ module RedirectOnBack
 
           def extra_tags_for_form(html_options)
             orig_tags = orig_extra_tags_for_form(html_options)
-            orig_tags << "<input name='_usec' type='hidden' value='#{Time.now.usec}' />".html_safe
+            unless html_options.delete(:disable_redirect_on_back)
+              orig_tags << "<input name='_usec' type='hidden' value='#{Time.now.usec}' />".html_safe
+            end
+            orig_tags
           end
         end
       end
